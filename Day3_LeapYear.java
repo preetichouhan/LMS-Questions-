9bff4452985ca8fb1481b899364c612e81d9cb46
Preeti Chouhan <89197259+preetichouhan@users.noreply.github.com>
2024-09-28 15:26:37 +0530

    Add files via upload

---
+++ Day3_LeapYear.java
@@ -0,0 +1,19 @@
+package Day3;
+import java.util.Scanner;
+
+public class Day3_LeapYear {
+
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.print("Enter a year: ");
+	        int year = scanner.nextInt();
+
+	        if (year % 4 == 0 && year % 100 != 0) {
+	            System.out.println(year + " is a leap year.");
+	        } else if (year % 400 == 0) {
+	            System.out.println(year + " is a leap year.");
+	        } else {
+	            System.out.println(year + " is not a leap year.");
+	        }
+	    }
+	}
